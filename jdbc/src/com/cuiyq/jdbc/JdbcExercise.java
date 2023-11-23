d2b29db26ee55763026101401ea1d4a69e3d40dd
Cuiyq110 <cyq1102051001@gmail.com>
2023-11-23 14:00:18 +0800

    学习jdbc查数据

--- jdbc/src/com/cuiyq/jdbc/JdbcExercise.java
+++ jdbc/src/com/cuiyq/jdbc/JdbcExercise.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -29,16 +31,28 @@ public class JdbcExercise {
         Class aClass = Class.forName(driver);
         Connection connection = DriverManager.getConnection(url, user, password);
         System.out.println("第五种方式:" + connection);
+        Statement statement = connection.createStatement();
 //        建表
         String sql1 = "CREATE TABLE new( id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,name varchar(25), tel varchar(25)) AUTO_INCREMENT = 1";
 //        添加数据
         String sql2 = "INSERT INTO new VALUES(null,'cuiyq','123'),(null,'杨莹','143'),(null,'fs','145')";
 //        删除表
         String sql3 = "drop table new";
-//        执行sql语句
-        Statement statement = connection.createStatement();
-        int rows = statement.executeUpdate(sql2);
-        System.out.println(rows > 0 ? "成功" : "失败");
+//        查询actor表
+        String sql4 = "select * from actor";
+        ResultSet resultSet = statement.executeQuery(sql4);
+        while (resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String sex = resultSet.getString(3);
+            Date borndate = resultSet.getDate(4);
+            String phone = resultSet.getString(5);
+            System.out.println(id + " \t" + name + " \t" + sex + " \t" + borndate + " \t" + phone);
+        }
+
+
+//        int rows = statement.executeUpdate(sql2);
+//        System.out.println(rows > 0 ? "成功" : "失败");
         System.out.println("执行完成");
 
         connection.close();
---
+++ jdbc/src/com/cuiyq/jdbc/statment/Statement.java
@@ -0,0 +1,9 @@
+package com.cuiyq.jdbc.statment;
+
+/**
+ * @author Cuiyq
+ * @version 1.0
+ * describe:
+ */
+public class Statement {
+}
