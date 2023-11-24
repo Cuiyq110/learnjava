b10bc273d32dfd3f326194e5cf4f3061976d8db0
Cuiyq110 <cyq1102051001@gmail.com>
2023-11-24 14:09:08 +0800

    使用工具类进行查询，增加数据

--- jdbc/src/com/cuiyq/jdbc/utils/JDBCUtils_Use.java
+++ jdbc/src/com/cuiyq/jdbc/utils/JDBCUtils_Use.java
@@ -1,20 +1,59 @@
 package com.cuiyq.jdbc.utils;
 
+import com.cuiyq.jdbc.JdbcConnect;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.Properties;
 
 /**
  * @author Cuiyq
  * @version 1.0
- * describe:
+ * describe: 使用utils类更新，添加，查询数据
  */
 
 @SuppressWarnings("all")
 public class JDBCUtils_Use {
+
+    @Test
+    public void testSelect() {
+//        得到连接
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+//        组织一个sql
+        String sql = "SELECT * FROM actor";
+
+        try {
+            connection = JDBCUtils.getConnection();
+            System.out.println(connection.getClass());
+            preparedStatement = connection.prepareStatement(sql);
+//            preparedStatement.setInt(1,2);
+//           执行得到结果集
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next())
+            {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String sex = resultSet.getString("sex");
+                Date borndate = resultSet.getDate("borndate");
+                String phone = resultSet.getString("phone");
+                System.out.println(id + " \t" + name + " \t" + sex + " \t" + borndate + " \t" + phone);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(resultSet,preparedStatement,connection);
+        }
+
+    }
+
+
 
     public static void main(String[] args) {
         new JDBCUtils_Use().testDML_update("王武",13);
