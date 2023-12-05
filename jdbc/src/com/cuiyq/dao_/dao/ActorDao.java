2e134944e4e779fa7634e6fc8411bc6403ff2a42
Cuiyq110 <cyq1102051001@gmail.com>
2023-12-05 16:09:48 +0800

    开发 BasicDAO , 是其他 DAO 的父类, 使用到 apache-dbutils

---
+++ jdbc/src/com/cuiyq/dao_/dao/ActorDao.java
@@ -0,0 +1,9 @@
+package com.cuiyq.dao_.dao;
+
+/**
+ * @author Cuiyq
+ * @version 1.0
+ * describe:
+ */
+public class ActorDao {
+}
---
+++ jdbc/src/com/cuiyq/dao_/test/TestDao.java
@@ -0,0 +1,9 @@
+package com.cuiyq.dao_.test;
+
+/**
+ * @author Cuiyq
+ * @version 1.0
+ * describe:
+ */
+public class TestDao {
+}
