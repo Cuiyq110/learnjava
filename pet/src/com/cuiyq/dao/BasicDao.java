f62bdd5861302f2071fc6c718444193204b6d636
Cuiyq110 <cyq1102051001@gmail.com>
2023-12-06 14:38:56 +0800

    完成BasicDao基本的增删改查，完成德鲁伊工具类和用户输入工具类

--- pet/src/com/cuiyq/dao/BasicDao.java
+++ pet/src/com/cuiyq/dao/BasicDao.java
@@ -1,11 +1,74 @@
 package com.cuiyq.dao;
 
+import com.cuiyq.utils.JDBCUtilsByDruid;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * @author Cuiyq
  * @version 1.0
- * describe: 完成最基本的功能
+ * describe: 开发BasicDao是其他dao的父类
  */
-public class BasicDao {
+@SuppressWarnings("all")
+public class BasicDao<T> {
+    private QueryRunner queryRunner = new QueryRunner();
 
+    //    返回多个对象，针对任意表
+    public List<T> queryMulti(String sql, Class<T> claszz, Object... parameters) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(claszz), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(connection, null, null);
+        }
+    }
 
+    //    返回单个对象
+    public T querySingle(String sql, Class<T> classzz, Object... parameters) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection, sql, new BeanHandler<T>(classzz), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(connection, null, null);
+        }
+    }
+
+    //    返回单行单列
+    public Object queryScalar(String sql, Object... parameters) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return queryRunner.query(connection,sql,new ScalarHandler(), parameters);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(connection,null,null);
+        }
+    }
+
+//    通用的dml方法,针对任意表
+    public int update(String sql,Object... parameters) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            int update = queryRunner.update(connection,sql,parameters);
+            return update;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.close(connection,null,null);
+        }
+    }
 }
---
+++ pet/src/com/cuiyq/utils/JDBCUtilsByDruid.java
@@ -0,0 +1,56 @@
+package com.cuiyq.utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author Cuiyq
+ * @version 1.0
+ * describe: 基于druid数据库连接池的工具类
+ */
+public class JDBCUtilsByDruid {
+
+    private static DataSource ds;
+
+    //    在静态代码块中完成ds初始化
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("src//druid.properties"));
+            ds = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //编写getConnection方法
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    //编写把Connection对象放回连接池的方法
+    public static void close(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
